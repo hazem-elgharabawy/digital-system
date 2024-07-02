5d0c26773e73f498358053c042379a43669c8105
Hazem Elgharabawy <hazem.gharabawy6@gmail.com>
2024-07-02 14:56:40 +0300

    updated UART_RX_deserializer

--- UART/UART_RX/UART_RX_deserializer.v
+++ UART/UART_RX/UART_RX_deserializer.v
@@ -1,7 +1,24 @@
 module deserializer (
-    input sampled_bit,
-    input deser_en,
-    output P_DATA
+    input           clk,
+    input           rst,
+    input           sampled_bit,
+    input           deser_en,
+    output [7:0]    P_DATA
 );
+
+    reg [7:0]data;
+    always @(posedge clk) begin
+        if (!rst) begin
+            data <= 0; 
+        end
+        else begin
+            if (deser_en) begin
+                data <= {data,sampled_bit};
+            end 
+        end     
+    end
     
+assign P_DATA = data;
+
+
 endmodule
