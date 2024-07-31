0ee1a030034deb5b9e25bb9c0dff13b5ee91ab41
Hazem Elgharabawy <hazem.gharabawy6@gmail.com>
2024-08-01 00:47:54 +0300

    DFT insertion

--- UART/UART_TX/UART_TOP.v
+++ UART/UART_TX/UART_TOP.v
@@ -6,7 +6,15 @@ module UART_TX_TOP #(
     input Data_Valid,
     input par_en, PAR_TYP,
     input [FRAME_WIDTH-1:0] P_Data,
-    output TX_OUT, busy
+    output TX_OUT, busy,
+    //DFT PORTS
+    input test_mode,
+    input scan_clk,
+    input scan_rst,
+    input SE,
+    input SI,
+    output SO
+    
 );
     
     //internal signals 
@@ -17,15 +25,46 @@ module UART_TX_TOP #(
     wire start_bit;
     wire stop_bit;
     wire ser_data;
-
+    // DFT signals
+    wire M_CLK,
+    wire M_RST,
 
     assign start_bit  = 0;
     assign stop_bit  = 1;
 
+    //DFT_insertion
+    mux2X1 mux_CLK (.IN_0(clk),.IN_1(scan_clk),.SEL(test_mode),.OUT(M_CLK));
+    mux2X1 mux_RST (.IN_0(reset),.IN_1(scan_rst),.SEL(test_mode),.OUT(M_RST));
+
     // instantiation
-    serializer serializer (.*);
-    parity_calc parity (.*);
-    FSM fsm (.*);
+    serializer serializer (
+        .clk(M_CLK),
+        .reset(M_RST), 
+        .ser_en(ser_en),
+        .Data_Valid(Data_Valid),
+        .P_Data(P_Data),
+        .ser_done(ser_done),
+        .ser_data(ser_data)
+    );
+    parity_calc parity (
+        .clk(M_CLK),
+        .reset(M_RST),
+        .Data_Valid(Data_Valid),
+        .PAR_TYP(PAR_TYP),
+        .P_Data(P_Data),
+        .par_bit(par_bit)
+    );
+
+    FSM fsm (
+        .clk(M_CLK),
+        .reset(M_RST),
+        .Data_Valid(Data_Valid),
+        .par_en(par_en),
+        .ser_done(ser_done),
+        .ser_en(ser_en),
+        .busy(busy),
+        .mux_sel(mux_sel)
+    );
     
     MUX mux (
         .IN0(ser_data),
